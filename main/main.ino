c67d216025058096fcae1ee28b2e135fd64a5ead
Tianjiao Huang <maitian519@gmail.com>
2017-10-27 09:24:42 -0700

    able to turn led on/off?

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,11 @@
 // Constants
-const int BaudRate = 9600; // Transfer rate in bits per second 
+const int BaudRate = 9600; // Transfer rate in bits per second
+const int ledPin = 13; 
 
 void setup()
 {
+    pinMode(ledPin, OUTPUT);
+
     // Open serial connection
     Serial.begin(BaudRate);
 }
@@ -13,12 +16,20 @@ void loop()
     if (Serial.available() <= 0)
         return;
 
+    // Read command
+    int command = Serial.read();    // read the next byte avilable; -1 if no data
+    
     // Send data to RPI
     sendData();
 
-        // Flush serial buffer  
+    // Flush serial buffer  
     while (Serial.available() > 0)
         Serial.read();
+}
+
+void parseCommand(int command)
+{
+    digitalWrite(ledPin, command ? HIGH : LOW);   // sets the LED on or off   
 }
 
 void sendData()
